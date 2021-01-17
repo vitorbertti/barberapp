5b5f76eaa82fefcb63b841a7dd520e920b11ea98
Vitor Bertti <berttivitor@gmail.com>
2021-01-17 18:46:30 -0300

    Added UserContextProvider

--- src/screens/Preload/indes.tsx
+++ src/screens/Preload/indes.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Container, LoadingIcon} from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useNavigation} from '@react-navigation/native';
 
 import BarberLogo from '../../assets/barber.svg';
 
 export default () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        console.log('validate');
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+    checkToken();
+  }, [navigation]);
+
   return (
     <Container>
       <BarberLogo width="100%" heigth="160" />
