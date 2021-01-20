90b73d4698556d4d22b6fcba0bea582bc55a6b28
Vitor Bertti <berttivitor@gmail.com>
2021-01-19 21:50:59 -0300

    Added SignIn styles

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -8,14 +8,38 @@ export const Container = styled.SafwAreaView`
   align-items: center;
 `;
 
-export const InputArea = styled.View``;
+export const InputArea = styled.View`
+  padding: 40px;
+`;
 
-export const CustomButtom = styled.TouchableOpacity``;
+export const CustomButtom = styled.TouchableOpacity`
+  height: 60px;
+  background-color: #268596;
+  border-radius: 30px;
+  justify-content: center;
+  align-items: center;
+`;
 
-export const CustomButtomText = styled.Text``;
+export const CustomButtomText = styled.Text`
+  font-size: 18px;
+  color: #fff;
+`;
 
-export const SignMessageButtom = styled.TouchableOpacity``;
+export const SignMessageButtom = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 50px;
+  margin-bottom: 20px;
+`;
 
-export const SignMessageButtomText = styled.Text``;
+export const SignMessageButtomText = styled.Text`
+  font-size: 16px;
+  color: #268596;
+`;
 
-export const SignMessageButtomTextBold = styled.Text``;
+export const SignMessageButtomTextBold = styled.Text`
+  font-size: 16px;
+  color: #268596;
+  font-weight: bold;
+  margin-left: 5px;
+`;
