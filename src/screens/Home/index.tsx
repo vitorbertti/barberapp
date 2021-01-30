8e4d973fd451b3f91b2405158f8a1d4a19a046b7
Vitor Bertti <berttivitor@gmail.com>
2021-01-29 21:39:41 -0300

    Added home page header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './styles';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
+import {useNavigation} from '@react-navigation/native';
 
 export default () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>
+            Encontre o seu barbeiro favorito
+          </HeaderTitle>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
+            <SearchIcon width="26" height="26" fill="#fff" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde você está?"
+            placeholderTextColor="#fff"
+          />
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill="#fff" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
     </Container>
   );
 };
