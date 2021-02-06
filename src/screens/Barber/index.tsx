93276cccfa19374a4aab3dc561bb4788ae783b6f
Vitor Bertti <berttivitor@gmail.com>
2021-02-06 18:09:25 -0300

    Added getBarberInfo function

--- src/screens/Barber/index.tsx
+++ src/screens/Barber/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, Text} from 'react-native';
 import {Container} from './styles';
+import {useNavigation, useRoute} from '@react-navigation/native';
+
+import Api from '../../Api';
 
 export default () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const [userInfo, setUserInfo] = useState({
+    id: route.params.id,
+    avatar: route.params.avatar,
+    name: route.params.name,
+    stars: route.params.stars,
+  });
+
+  useEffect(() => {
+    const getBarberInfo = async () => {
+      let json = await Api.getBarber(userInfo.id);
+      if (json.error === '') {
+        setUserInfo(json.data);
+      } else {
+        Alert.alert('Erro', json.error, [], {cancelable: true});
+      }
+    }
+    getBarberInfo();
+  }, [userInfo.id]);
+
   return (
     <Container>
-      <Text>Barber</Text>
+      <Text>Barbeiro: {userInfo.name}</Text>
     </Container>
   );
 };
